96091400df8c63c6387b8e0440298a728c3f8896
Yuriy Yevstihnyeyev <yuriy@vaadin.com>
2018-04-05 13:43:54 +0300

    Work with p3 versions of vaadin-text-field and vaadin-button

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 import Vue from 'vue'
+
+import '@webcomponents/webcomponentsjs/webcomponents-loader.js';
+import '@vaadin/vaadin-button/vaadin-button.js';
+import '@vaadin/vaadin-text-field/vaadin-text-field.js';
+
 import App from './App.vue'
 
 Vue.config.ignoredElements = [
