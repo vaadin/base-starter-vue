92158898ba6e97d6ede360599b4a3d4cfd21c69c
Yuriy Yevstihnyeyev <yuriy@vaadin.com>
2018-04-06 10:51:22 +0300

    Implement review suggestions

--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
 import Vue from 'vue'
 
 import '@webcomponents/webcomponentsjs/webcomponents-loader.js';
-import '@vaadin/vaadin-button/vaadin-button.js';
-import '@vaadin/vaadin-text-field/vaadin-text-field.js';
 
 import App from './App.vue'
 
